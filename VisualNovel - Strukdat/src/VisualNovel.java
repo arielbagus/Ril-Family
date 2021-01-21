549d63c393b7205905130620aa462c7b708f480c
user <user@DESKTOP-5GHSL3J>
2021-01-21 13:21:10 +0700

    Ahmad Jibril Muayyad Commit

--- VisualNovel - Strukdat/src/VisualNovel.java
+++ VisualNovel - Strukdat/src/VisualNovel.java
@@ -2,6 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,19 +17,19 @@ public class VisualNovel {
 
 	JFrame window;
 	Container con;
-	JPanel titleNamePanel, startButtonPanel, mainTextPanel ;
-	JLabel titleNameLabel, pictureLabel;
+	JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel ;
+	JLabel titleNameLabel, pictureLabel, pictureLabel2;
 	Font titleFont = new Font("Times New Roman", Font.PLAIN, 60);
+	Font NormalFont = new Font("Times New Roman", Font.PLAIN, 28);
 	Font startButtonFont = new Font("Times New Roman", Font.PLAIN, 28);
-	JButton startButton, nextButton;
+	JButton startButton, nextButton, choice1, choice2, choice3, choice4;
 	JTextArea mainTextArea;
 	ImageIcon picture;
+	//ImageIcon picture2;
+	
+	TitleScreenHandler tsHandler = new TitleScreenHandler();
 	
 	public VisualNovel() {
-		
-		picture = new ImageIcon(this.getClass().getResource("/01.png"));
-		pictureLabel = new JLabel(picture);
-		pictureLabel.setSize(800, 600);
 		
 		window = new JFrame("Visual Novel");
 		window.setSize(800, 600);
@@ -35,13 +38,24 @@ public class VisualNovel {
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		con = window.getContentPane();
-		
+	
+		//picture = new ImageIcon(this.getClass().getResource("/01.png"));
+		//picture2 = new ImageIcon(this.getClass().getResource("/03-1.png"));
+		pictureLabel = new JLabel(picture);
+		pictureLabel.setSize(800, 600);		
+
 		titleNamePanel = new JPanel(new BorderLayout());
-		titleNamePanel.setBounds(100, 100, 600, 150);
+		titleNamePanel.setBounds(100, 45, 600, 400);
 		titleNamePanel.setOpaque(false);
-		titleNameLabel = new JLabel("PERCOBAAN SATU");
+		titleNameLabel = new JLabel();
+		titleNameLabel.setText("The Legend Of Irsyad");
+		//titleNameLabel.setIcon(picture2);
+		titleNameLabel.setHorizontalTextPosition(JLabel.CENTER);
+		titleNameLabel.setVerticalTextPosition(JLabel.TOP);
+		titleNameLabel.setIconTextGap(40);
 		titleNameLabel.setForeground(Color.BLACK);
 		titleNameLabel.setFont(titleFont);
+		titleNameLabel.setVerticalAlignment(JLabel.CENTER);
 		titleNameLabel.setHorizontalAlignment(JLabel.CENTER);
 		
 		startButtonPanel = new JPanel(new BorderLayout());
@@ -50,12 +64,12 @@ public class VisualNovel {
 		
 		startButton = new JButton("Mulai");
 		startButton.setFont(startButtonFont);
-		startButton.setOpaque(false);
-		startButton.setContentAreaFilled(false);
-		startButton.setBorderPainted(false);
 		startButton.setForeground(Color.black);
-		startButton.setFocusPainted(false);
-
+		startButton.setBackground(Color.white);
+		startButton.addActionListener(tsHandler);
+		//startButton.setOpaque(false);
+		//startButton.setContentAreaFilled(false);
+		//startButton.setBorderPainted(false);
 		
 		titleNamePanel.add(titleNameLabel);
 		startButtonPanel.add(startButton);
@@ -68,12 +82,59 @@ public class VisualNovel {
 	
 	public void createGameScreen() {
 		
+		titleNamePanel.setVisible(false);
+		startButtonPanel.setVisible(false);
+		pictureLabel.setVisible(false);
+		
 		mainTextPanel = new JPanel();
 		mainTextPanel.setBounds(100, 100, 600, 250); 
-		mainTextPanel.setBackground(Color.blue);
+		mainTextPanel.setBackground(Color.black);
 		con.add(mainTextPanel);
 		
+		mainTextArea = new JTextArea("This is the main text are. This game is going to be great. I'm sure of it!!!!!!!");
+		mainTextArea.setBounds(100, 100, 600, 250);
+		mainTextArea.setBackground(Color.BLACK);
+		mainTextArea.setForeground(Color.WHITE);
+		mainTextArea.setFont(startButtonFont);
+		mainTextArea.setLineWrap(true);
+		mainTextPanel.add(mainTextArea);
 		
+		choiceButtonPanel = new JPanel();
+		choiceButtonPanel.setBounds(250, 350, 300, 150);
+		choiceButtonPanel.setBackground(Color.black);
+		choiceButtonPanel.setLayout(new GridLayout(4,1));
+		con.add(choiceButtonPanel);
+		
+		choice1 = new JButton("Choice 1");
+		choice1.setBackground(Color.black);
+		choice1.setForeground(Color.white);
+		choice1.setFont(NormalFont);
+		choiceButtonPanel.add(choice1);
+		choice2 = new JButton("Choice 2");
+		choice2.setBackground(Color.black);
+		choice2.setForeground(Color.white);
+		choice2.setFont(NormalFont);
+		choiceButtonPanel.add(choice2);
+		choice3 = new JButton("Choice 3");
+		choice3.setBackground(Color.black);
+		choice3.setForeground(Color.white);
+		choice3.setFont(NormalFont);
+		choiceButtonPanel.add(choice3);
+		choice4 = new JButton("Choice 4");
+		choice4.setBackground(Color.black);
+		choice4.setForeground(Color.white);
+		choice4.setFont(NormalFont);
+		choiceButtonPanel.add(choice4);
+		
+	}
+	
+	public class TitleScreenHandler implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event) {
+			
+			createGameScreen();
+			
+		}
 	}
 	
 	public static void main(String[] args) {
